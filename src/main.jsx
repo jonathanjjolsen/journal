b305060d5d66abda462fd0969573e6465a1e803d
Jonathan <jonathanjolsen@gmail.com>
2024-07-07 15:59:08 -0700

    got everything back to kind of working. having issues launching server and client at the same time.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloProvider } from '@apollo/client'
-import client from './apolloClient'
+import client from './apolloClient.js'
 import App from './App.jsx'
 import './index.css'
 
